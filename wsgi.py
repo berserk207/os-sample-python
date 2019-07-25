b50c5da1bd5fc34b89ca074a3b6482ed5b6e5e6c
berserk207 <clark56@gmail.com>
2019-07-25 14:26:19 +0800

    Update wsgi.py
    added sleep

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
+import time
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    time.sleep( 60 )
     return "OpenShift Hello World!"
 
 if __name__ == "__main__":
